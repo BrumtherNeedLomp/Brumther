9c9b17d1e2386d2c610c85ad9b48ea71df2a6963
ChaytonL1 <42649148+ChaytonL1@users.noreply.github.com>
2018-10-11 10:35:18 -0500

    shrek heck

--- RpgLamp/RpgLamp/Main.cpp
+++ RpgLamp/RpgLamp/Main.cpp
@@ -1,135 +1,177 @@
-#include <iostream>
 #include <string>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <time.h>
 #define HEADER_H_
 #include "Header.h"
+#include <stdio.h>     
+#include <stdlib.h>
 
 using namespace std;
+
+
+int skillSet(int sklPts, string name, int sklChoice, int skillPointInput, int xp) {
+
+
+	int skill[6] = { 10, 10, 10, 10, 10, 0 };
+
+	do {
+
+
+		cout << name << ", You have " << sklPts << " skill points left, what skill would you like to upgrade." << endl;
+		cout << "1 = HP\n2 = Power\n3 = Agility\n4 = Stamina\n5 = Charisma\n6 = Lumpther (Ultimate)" << endl;
+		cin >> sklChoice;
+		if (sklChoice == 1)
+		{
+			cout << "How many points would you like to put into Power?\n >> ";
+			cin >> skillPointInput;
+			if (skillPointInput > sklPts)
+			{
+				cout << "You have " << sklPts << " , you need " << skillPointInput << endl;
+			}
+			else if (skillPointInput < 0)
+			{
+				cout << "Error not possible!";
+			}
+			else
+			{
+				skill[0] = skill[0] + skillPointInput;
+				sklPts = sklPts - skillPointInput;
+			}
+
+		}
+		else if (sklChoice == 2)
+		{
+			cout << "How many points would you like to put into Power?\n >> ";
+			cin >> skillPointInput;
+			if (skillPointInput > sklPts)
+			{
+				cout << "You have " << sklPts << " , you need " << skillPointInput << endl;
+			}
+			else if (skillPointInput < 0)
+			{
+				cout << "Error not possible!";
+			}
+			else
+			{
+				skill[1] = skill[1] + skillPointInput;
+				sklPts = sklPts - skillPointInput;
+			}
+
+		}
+		else if (sklChoice == 3)
+		{
+			cout << "How many points would you like to put into Agilty?\n >> ";
+			cin >> skillPointInput;
+			if (skillPointInput > sklPts)
+			{
+				cout << "You have " << sklPts << " , you need " << skillPointInput << endl;
+			}
+			else if (skillPointInput < 0)
+			{
+				cout << "Error not possible!";
+			}
+			else
+			{
+				skill[2] = skill[2] + skillPointInput;
+				sklPts = sklPts - skillPointInput;
+			}
+		}
+		else if (sklChoice == 4)
+		{
+			cout << "How many points would you like to put into Stamina?\n >> ";
+			cin >> skillPointInput;
+			if (skillPointInput > sklPts)
+			{
+				cout << "You have " << sklPts << " , you need " << skillPointInput << endl;
+			}
+			else if (skillPointInput < 0)
+			{
+				cout << "Error not possible!";
+			}
+			else
+			{
+				skill[3] = skill[3] + skillPointInput;
+				sklPts = sklPts - skillPointInput;
+			}
+		}
+		else if (sklChoice == 5)
+		{
+			cout << "How many points would you like to put into Chraisma?\n >> ";
+			cin >> skillPointInput;
+			if (skillPointInput > sklPts)
+			{
+				cout << "You have " << sklPts << " , you need " << skillPointInput << endl;
+			}
+			else if (skillPointInput < 0)
+			{
+				cout << "Error not possible!";
+			}
+			else
+			{
+				skill[4] = skill[4] + skillPointInput;
+				sklPts = sklPts - skillPointInput;
+			}
+		}
+		else if (sklChoice == 6 && xp >= 50)
+		{
+			cout << "How many points would you like to put into Lumpther?\n >> ";
+			cin >> skillPointInput;
+			if (skillPointInput > sklPts)
+			{
+				cout << "You have " << sklPts << " , you need " << skillPointInput << endl;
+			}
+			else if (skillPointInput < 0)
+			{
+				cout << "Error not possible!";
+			}
+			else
+			{
+				skill[5] = skill[5] + skillPointInput;
+				sklPts = sklPts - skillPointInput;
+			}
+		}
+		else
+		{
+			cout << "Big Gay";
+		}
+	} while (sklPts != 0);
+	cout << skill[0] << " - HP" << endl << skill[1] << " - Power" << endl << skill[2] << " - Agility" << endl << skill[3] << " - Stamina" << endl << skill[4] << " - Chrasima" << endl << skill[5] << " - Lumpther" << endl;
+
+	return 0;
+}
+
+
+
 
 int main()
 {
 	string name;
-	
+
 	int xp = 0;
 	int gold = 0;
-	int hp = 10;
 	int lomps = 0;
 	int sklPts = 10;
-	int sklChoice;
-	int skillPointInput;
+	int sklChoice = 0;
+	int skillPointInput = 0;
 	int goBack;
 
-	int skill[5] = {10, 10, 10, 10, 0};
+
 
 	cout << "What is your name Stranger Moth?\n >> ";
 	cin >> name;
-	
-	
 
-		do {
-			cout << name << ", You have " << sklPts << " skill points left, what skill would you like to upgrade." << endl;
-			cout << "1 = Power\n2 = Agility\n3 = Stamina\n4 = Charisma\n5 = Lumpther (Ultimate)" << endl;
-			cin >> sklChoice;
-			if (sklChoice == 1)
-			{
-				cout << "How many points would you like to put into Power?\n >> ";
-				cin >> skillPointInput;
-				if (skillPointInput > sklPts)
-				{
-					cout << "You have " << sklPts << " , you need " << skillPointInput << endl;
-				}
-				else if (skillPointInput < 0)
-				{
-					cout << "Error not possible!";
-				}
-				else 
-				{
-					skill[0] = skill[0] + skillPointInput;
-					sklPts = sklPts - skillPointInput;
-				}
-				
-			}
-			 else if (sklChoice == 2)
-			{
-				 cout << "How many points would you like to put into Agilty?\n >> ";
-				 cin >> skillPointInput;
-				 if (skillPointInput > sklPts)
-				 {
-					 cout << "You have " << sklPts << " , you need " << skillPointInput << endl;
-				 }
-				 else if (skillPointInput < 0)
-				 {
-					 cout << "Error not possible!";
-				 }
-				 else
-				 {
-					 skill[1] = skill[1] + skillPointInput;
-					 sklPts = sklPts - skillPointInput;
-				 }
-			}
-			 else if (sklChoice == 3)
-			 {
-				 cout << "How many points would you like to put into Stamina?\n >> ";
-				 cin >> skillPointInput;
-				 if (skillPointInput > sklPts)
-				 {
-					 cout << "You have " << sklPts << " , you need " << skillPointInput << endl;
-				 }
-				 else if (skillPointInput < 0)
-				 {
-					 cout << "Error not possible!";
-				 }
-				 else
-				 {
-					 skill[2] = skill[2] + skillPointInput;
-					 sklPts = sklPts - skillPointInput;
-				 }
-			 }
-			 else if (sklChoice == 4)
-			 {
-				 cout << "How many points would you like to put into Chraisma?\n >> ";
-				 cin >> skillPointInput;
-				 if (skillPointInput > sklPts)
-				 {
-					 cout << "You have " << sklPts << " , you need " << skillPointInput << endl;
-				 }
-				 else if (skillPointInput < 0)
-				 {
-					 cout << "Error not possible!";
-				 }
-				 else
-				 {
-					 skill[3] = skill[3] + skillPointInput;
-					 sklPts = sklPts - skillPointInput;
-				 }
-			 }
-			 else if (sklChoice == 5 && xp >= 50)
-			 {
-				 cout << "How many points would you like to put into Lumpther?\n >> ";
-				 cin >> skillPointInput;
-				 if (skillPointInput > sklPts)
-				 {
-					 cout << "You have " << sklPts << " , you need " << skillPointInput << endl;
-				 }
-				 else if (skillPointInput < 0)
-				 {
-					 cout << "Error not possible!";
-				 }
-				 else
-				 {
-					 skill[4] = skill[4] + skillPointInput;
-					 sklPts = sklPts - skillPointInput;
-				 }
-			 }
 
-			else
-			{
-				cout << "Big Gay";
-			}
-		} while (sklPts != 0);
+	skillSet(sklPts, name, sklChoice, skillPointInput, xp);
 
-		cout << skill[0] << " - Power" << endl << skill[1] << " - Agility" << endl << skill[2] << " - Stamina" << endl << skill[3] << " - Chrasima" << endl << skill[4] << " - Lumpther" << endl;
-		randUno();
+
+	cout << "The Moth King, King of all LAMP, approaches you. He asks you to go to the cave and 'defeet' the monster there!\n";
+	cout << "The king tells you it is south, so you go on your way.\n";
+	cout << "As you enter the cave you here strage noises\n";
+	randUno();
+
 	system("pause");
 	return 0;
-	
+
 }
--- RpgLamp/RpgLamp/random.cpp
+++ RpgLamp/RpgLamp/random.cpp
@@ -5,29 +5,34 @@
 #include <time.h>
 #define HEADER_H_
 #include "Header.h"
-#include <stdio.h>      /* printf, NULL */
+#include <stdio.h>     
 #include <stdlib.h>
 
 using namespace std;
 
+int spookyLamp[5] = { 15 , 6, 15, 6, 2 };
+int dangerNoodle[5] = { 5, 20, 11, 2, 2 };
+int pokeyfish[5] = { 20, 5, 2, 5, 2,};
+int indianMan[5] = { 5, 5, 5, 5, 25 };
+int swegsavagejr[5] = { 50, 1, 1, 1, 1 };
+int bowsette[5] = { 20, 3, 2, 5, 15 };
+int chocomalk[5] = { 10, 5, 2, 10, 2 };
+int lordFarquad[5] = { 28, 6, 4, 8, 21 };
+int generator;
+
 int randUno() {
 
-	int dangerNoodle;
-	int spookylamp;
-	int pokeyfish;
-	int indianMan;
-	int swegsavagejr;
-	int bowsette;
-	int chocomalk;
-	int generator;
+
+
+	
 
 	srand(time(NULL));
-	generator =1+ (rand() % 10);
+	generator = 1 + (rand() % 14);
 
-	if (generator == 1 || generator == 6) {
+	if (generator == 1 || generator == 8) {
 
 
-		string w = " You have encountered the the dangerous of all the noodles, THE DANGERNOODLE.\n";
+		string w = "You have encountered the the dangerous of all the noodles, THE DANGERNOODLE.\n";
 		for (int q = 0; q< w.size(); q++)
 		{
 			for (int e = 0; e <= 19999999; e++);
@@ -35,7 +40,7 @@ int randUno() {
 		}
 	}
 
-	else if (generator == 2 || generator == 7) {
+	else if (generator == 2 || generator == 9) {
 
 		string t = "SPOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOKY L-A-M-P, has turned on.\n";
 		for (int r = 0; r< t.size(); r++)
@@ -44,36 +49,54 @@ int randUno() {
 			cout << t[r];
 		}
 	}
-	else if (generator == 3 || generator == 8) {
+	else if (generator == 3 || generator == 10) {
 
-		string s = " A shark looking fish with a sword on the smeller, it goes by POKEYFISH.\n";
+		string s = "A shark looking fish with a sword on the smeller, it goes by POKEYFISH.\n";
 		for (int a = 0; a< s.size(); a++)
 		{
 			for (int d = 0; d <= 19999999; d++);
 			cout << s[a];
 		}
 	}
-	else if (generator == 4 || generator == 9) {
+	else if (generator == 4 || generator == 11) {
 
-		string x = " A dark figure stalking the internet on compooter... THE INDIAN MAN\n";
+		string x = "A dark figure stalking the internet on compooter... THE INDIAN MAN\n";
 		for (int z = 0; z< x.size(); z++)
 		{
 			for (int c = 0; c <= 19999999; c++);
 			cout << x[z];
 		}
 	}
-	else if (generator == 5 || generator == 10) {
+	else if (generator == 5 || generator == 12) {
 
-		string j = " You hear a squeeking noise in the distance... as you come closer you find out it's just Joey talking.. SWEGSAVAGEJR.\n";
-		for (int k = 0; k< j.size(); k++)
+		string j = "You hear a squeeking noise in the distance... as you come closer you find out it's just Joey talking.. SWEGSAVAGEJR.\n";
+		for (int k = 0; k < j.size(); k++)
 		{
 			for (int l = 0; l <= 19999999; l++);
 			cout << j[k];
 		}
 	}
+	else if (generator == 6 || generator == 13) {
+
+		string u = "You hear a scream for milk... ""OH HE NEEDS SOME MILK"" but not any milk some.... CHOCOMALK!!!\n";
+		for (int i = 0; i < u.size(); i++)
+		{
+			for (int o = 0; o <= 19999999; o++);
+			cout << u[i];
+		}
+	}
+	else if (generator == 7 || generator == 14) {
+
+		string v = " You hear your laptop sreaming... it's coming from your 'bowser' history....... BOWSETTE!!!\n";
+		for (int b = 0; b< v.size(); b++)
+		{
+			for (int n = 0; n <= 19999999; n++);
+			cout << v[b];
+		}
+	}
 
 
 
-	system("pause");
+
 	return 0;
 }
