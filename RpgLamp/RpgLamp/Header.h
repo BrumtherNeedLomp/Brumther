f364d91aa47800aad63117d2f314585f6545368e
ChaytonL1 <42649148+ChaytonL1@users.noreply.github.com>
2018-10-17 10:59:38 -0500

    LAOMPERTHERS

--- RpgLamp/RpgLamp/Header.h
+++ RpgLamp/RpgLamp/Header.h
@@ -3,11 +3,24 @@
 #include "Header.h"
 #define HEADER_H_
 
+int skill[6] = { 10, 10, 5, 4, 5, 0 };
 int randUno();
 int getNum();
-int getSkill0();
-int getSkill1();
-int getSkill2();
-int getSkill3();
-int getSkill3();
-int getSkill4();
+int getSkill0() {
+	return skill[0];
+}
+int getSkill1() {
+	return skill[1];
+}
+int getSkill2() {
+	return skill[2];
+}
+int getSkill3() {
+	return skill[3];
+}
+int getSkill4() {
+	return skill[4];
+}
+int getSkill5() {
+	return skill[5];
+}
--- RpgLamp/RpgLamp/Main.cpp
+++ RpgLamp/RpgLamp/Main.cpp
@@ -16,30 +16,7 @@ int user;
 
 int skill[6] = { 10, 10, 5, 4, 5, 0 };
 
-int getSkill0()
-{
-	return skill[0];
-}
-int getSkill1()
-{
-	return skill[1];
-}
-int getSkill2()
-{
-	return skill[2];
-}
-int getSkill3()
-{
-	return skill[3];
-}
-int getSkill4()
-{
-	return skill[4];
-}
-int getSkill5()
-{
-	return skill[5];
-}
+
 
 int skillSet(int sklPts, string name, int sklChoice, int skillPointInput, int xp) {
 
@@ -288,12 +265,32 @@ int quest()
 	{
 		cout << "why broke";
 	}
-
-
-
 	return 0;
 }
-
+int getSkill0()
+{
+	return skill[0];
+}
+int getSkill1()
+{
+	return skill[1];
+}
+int getSkill2()
+{
+	return skill[2];
+}
+int getSkill3()
+{
+	return skill[3];
+}
+int getSkill4()
+{
+	return skill[4];
+}
+int getSkill5()
+{
+	return skill[5];
+}
 
 
 int main()
